fc6c927d1044acaf4b313a997c4693f56f9f30e2
Fernando Fragoso <fernandofragoso@gmail.com>
2018-09-05 02:14:13 -0300

    Remove log and refactor constants

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const { getAuthor, getCategoriesArray, getItemsArray, getItem } = require('./uti
 
 const app = express();
 
-const SEARCH_URL = 'https://api.mercadolibre.com/sites/MLB/search?limit=4&q=';
-const ITEM_URL = 'https://api.mercadolibre.com/items/';
+const LIMIT = 4;
+const URL = 'https://api.mercadolibre.com/';
+const SEARCH_URL = `${URL}sites/MLB/search?limit=${LIMIT}&q=`;
+const ITEM_URL = `${URL}items`;
+const DESCRIPTION = 'description';
 
 app.get('/api/items', (req, res) => {
-  console.log('/api/items');
   const q = req.query.q;
   fetch(`${SEARCH_URL}${q}`)
     .then(res => res.json())
@@ -24,8 +26,8 @@ app.get('/api/items', (req, res) => {
 
 app.get('/api/items/:id', async (req, res) => {
   const id = req.params.id;
-  const { plain_text } = await fetch(`${ITEM_URL}${id}/description`).then(res => res.json());
-  fetch(`${ITEM_URL}${id}`)
+  const { plain_text } = await fetch(`${ITEM_URL}/${id}/${DESCRIPTION}`).then(res => res.json());
+  fetch(`${ITEM_URL}/${id}`)
     .then(res => res.json())
     .then(json => {
       const response = {
